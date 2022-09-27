b07abfd9e37df22227afe3e562d8a79bbe75e973
zachmanac <zachstephenson94@gmail.com>
2022-09-27 22:19:05 +0000

    typewriter settimeout created

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs\n";
+
+let delay = 0;
+
+for (const char of sentence) {
+  
+  setTimeout(() => {
+    process.stdout.write(char)
+  }, delay);
+  delay += 50;
+  
+};
